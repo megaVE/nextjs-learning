230a6f2844f2af7ef788136be1c20d051eb66dfd
megaVE <vinicius.honorio@polygonsolucoes.com.br>
2024-09-20 15:35:17 -0300

    Pending Mutating Data

--- 05-mutating-data/app/new-post/page.js
+++ 05-mutating-data/app/new-post/page.js
@@ -1,46 +1,35 @@
-import { storePost } from '@/lib/posts';
+import { storePost } from "@/lib/posts";
+import { redirect } from "next/navigation";
+import PostForm from "@/components/post-form";
+
+const isInvalidText = (text) => !text || text.trim().length === 0;
 
 export default function NewPostPage() {
-  async function createPost(formData) {
+  async function createPost(_, formData) {
     "use server";
-    const title = formData.get('title');
-    const image = formData.get('image');
-    const content = formData.get('content');
+    const title = formData.get("title");
+    const image = formData.get("image");
+    const content = formData.get("content");
 
-    storePost({
-      imageUrl: '',
+    let errors = [];
+
+    if (isInvalidText(title)) errors.push("Title is required.");
+
+    if (isInvalidText(content)) errors.push("Content is required.");
+
+    if (!image || !image.size) errors.push("An image is required.");
+
+    if (errors.length) return { errors };
+
+    await storePost({
+      imageUrl: "",
       title,
       content,
-      userId: 1
-    })
+      userId: 1,
+    });
+
+    redirect("/feed");
   }
 
-  return (
-    <>
-      <h1>Create a new post</h1>
-      <form action={createPost}>
-        <p className="form-control">
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
-        </p>
-        <p className="form-control">
-          <label htmlFor="image">Image URL</label>
-          <input
-            type="file"
-            accept="image/png, image/jpeg"
-            id="image"
-            name="image"
-          />
-        </p>
-        <p className="form-control">
-          <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows="5" />
-        </p>
-        <p className="form-actions">
-          <button type="reset">Reset</button>
-          <button>Create Post</button>
-        </p>
-      </form>
-    </>
-  );
+  return <PostForm action={createPost} />;
 }
--- 05-mutating-data/components/posts.js
+++ 05-mutating-data/components/posts.js
@@ -1,5 +1,5 @@
-import { formatDate } from '@/lib/format';
-import LikeButton from './like-icon';
+import { formatDate } from "@/lib/format";
+import LikeButton from "./like-icon";
 
 function Post({ post }) {
   return (
@@ -12,7 +12,7 @@ function Post({ post }) {
           <div>
             <h2>{post.title}</h2>
             <p>
-              Shared by {post.userFirstName} on{' '}
+              Shared by {post.userFirstName} on{" "}
               <time dateTime={post.createdAt}>
                 {formatDate(post.createdAt)}
               </time>
